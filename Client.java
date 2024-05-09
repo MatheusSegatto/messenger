419b55091f09977f095bd91cec2f7cedf720fe26
Matheus Segatto <matheussegatto34@outlook.com>
2024-05-09 20:30:44 -0300

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,17 +1,28 @@
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.io.IOException;
 
 public class Client {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        HttpClient client = HttpClient.newHttpClient();
+    private HttpClient client;
+
+    // Construtor
+    public Client() {
+        this.client = HttpClient.newHttpClient();
+    }
+
+    public void sendRequest() throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("http://localhost:8000/hello"))
                 .build();
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println("Response from server: " + response.body());
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Client client = new Client();
+        client.sendRequest();
     }
 }
