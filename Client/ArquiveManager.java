4a5d1ca32e0a988435cc21e0f27aad3843a5ca8d
Adriano Gomes da Cunha <106453765+adrianogcunha@users.noreply.github.com>
2024-05-31 01:09:02 -0300

    Possibilidade de solucao para concorrencia

--- Client/ArquiveManager.java
+++ Client/ArquiveManager.java
@@ -5,26 +5,41 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.TreeMap;
 
 import Model.Mensagem;
 import Model.User;
 import Util.dataTools;
 
 public class ArquiveManager {
-    public static void writeFile(User userConected, Mensagem message) {
 
-        String fileName = userConected.getId();
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
-            String content;
-            if (message.getRemetente().equals(userConected.getUsername())){
-                content = "[VOCÊ] [" + dataTools.setSecondsToData(message.getTimestamp()) + "]: " + message.getContent();
-            }else{
-                content = "[" + message.getRemetente() + "] [" + dataTools.setSecondsToData(message.getTimestamp()) + "]" + message.getContent();
+    private static TreeMap<Long, Mensagem> waitingToBeWritten = new TreeMap<>();
+
+    public static void addMessageToBeWriten(Mensagem messageToBeWritten) throws InterruptedException {
+        waitingToBeWritten.put(messageToBeWritten.getTimestamp(), messageToBeWritten);
+        writeFile();
+    }
+
+    private static void writeFile() throws InterruptedException {
+        Thread.sleep(5000);
+        
+        Mensagem message = waitingToBeWritten.get(waitingToBeWritten.firstKey());
+        if (!waitingToBeWritten.isEmpty()){    
+            User userConected = ClientHandler.getUserConected();
+            String fileName = userConected.getId();
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+                String content;
+                if (message.getRemetente().equals(userConected.getUsername())){
+                    content = "[VOCÊ] [" + dataTools.setSecondsToData(message.getTimestamp()) + "]: " + message.getContent();
+                }else{
+                    content = "[" + message.getRemetente() + "] [" + dataTools.setSecondsToData(message.getTimestamp()) + "]" + message.getContent();
+                }
+                writer.write(content);
+                writer.newLine();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            writer.write(content);
-            writer.newLine();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
     
